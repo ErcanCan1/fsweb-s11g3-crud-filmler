25f4333b390a4007b933cc1b3a62cf5ba41af274
Ercan <ercann.6191@gmail.com>
2024-04-22 12:34:18 +0300

    fdsf

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from "react";
 
-import { Route, Switch, Redirect } from "react-router-dom";
+import { Route, Switch, Redirect, useHistory } from "react-router-dom";
 import MovieList from './components/MovieList';
 import Movie from './components/Movie';
-
+import EditMovieForm from "./components/EditMovieForm";
 import MovieHeader from './components/MovieHeader';
 
 import FavoriteMovieList from './components/FavoriteMovieList';
@@ -13,7 +13,7 @@ import axios from 'axios';
 const App = (props) => {
   const [movies, setMovies] = useState([]);
   const [favoriteMovies, setFavoriteMovies] = useState([]);
-
+const { push } = useHistory()
   useEffect(() => {
     axios.get('http://localhost:9000/api/movies')
       .then(res => {
@@ -25,6 +25,12 @@ const App = (props) => {
   }, []);
 
   const deleteMovie = (id) => {
+    axios
+    .delete(`http://localhost:9000/api/movies/${id}`)
+    .then(res => {
+      push("/movies");
+      setMovies(res.data)})
+    .catch(err => {console.log(err)});
   }
 
   const addToFavorites = (movie) => {
@@ -36,7 +42,7 @@ const App = (props) => {
       <nav className="bg-zinc-800 px-6 py-3">
         <h1 className="text-xl text-white">HTTP / CRUD Film Projesi</h1>
       </nav>
-
+     
       <div className="max-w-4xl mx-auto px-3 pb-4">
         <MovieHeader />
         <div className="flex flex-col sm:flex-row gap-4">
@@ -44,10 +50,11 @@ const App = (props) => {
 
           <Switch>
             <Route path="/movies/edit/:id">
+            <EditMovieForm setMovies= {setMovies}/>
             </Route>
 
             <Route path="/movies/:id">
-              <Movie />
+              <Movie deleteMovie={deleteMovie}/>
             </Route>
 
             <Route path="/movies">
